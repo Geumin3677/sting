3385b2c018a49ec88aa6c303ade2db641c6b6add
ABELA <91381965+Geumin3677@users.noreply.github.com>
2023-08-10 00:53:02 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
-  // customWorkerDir: 'worker',
-  // register: true,
-  // skipWaiting: true,
-  output: 'export',
+  reactStrictMode: true,
 })
 
-module.exports = withPWA()
+module.exports = withPWA(this.output='export')
